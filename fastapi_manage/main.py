e962f5b662c62dc0c5b2d98acf148180d793a32a
Ji-Yang Lin <xuunnis123@gmail.com>
2021-04-28 10:58:33 +0800

    AJAX with Fastapi to figure out CRUD with template

--- fastapi_manage/main.py
+++ fastapi_manage/main.py
@@ -62,176 +62,93 @@ async def main(request: Request):
     print("main")
     query = notes.select()
     all_item=await database.fetch_all(query)
-    return templates.TemplateResponse("code_list.html", {"request": request,"all_item":all_item})
+    return templates.TemplateResponse("main.html", {"request": request,"all_item":all_item})
 
 
-@app.post('/register',response_class=HTMLResponse)
-async def create(request:Request,user:str=Form(...),expired:str=Form(...),mac_address:str=Form(...)):
-    print("creating")
-    print("user:",user)
-    code_arg = expired
-    code_arg+= mac_address
-    encode=encode_rsa(code_arg,mac_address)
+@app.post('/register')
+async def create(code:CodeIn):
+
+    code_arg = code.expired
+    code_arg+= code.mac_address
+    encode=encode_rsa(code_arg,code.mac_address)
     #code.code=encode
     query=notes.insert().values(
-        user=user,
-        expired=expired,
+        user=code.user,
+        expired=code.expired,
         code=encode,
-        mac_address=mac_address
+        mac_address=code.mac_address
     )
     record_id=await database.execute(query)
-    return RedirectResponse(url='/',status_code=status.HTTP_302_FOUND)
+    return {
+        "code":"ok",
+        "message":"success"
+    }
     #return templates.TemplateResponse('index.html',{'request': request})
 
 
 
-@app.get("/create")
-async def create_form(request:Request):
-    print("create")
-    return templates.TemplateResponse('code_form.html',{"request":request,"notes":all_item})
-
-@app.get("/register/")
-async def read_notes(request:Request):
-    print("read")
-    query = notes.select()
-    all_item=await database.fetch_all(query)
-    return templates.TemplateResponse('code_list.html',{"request":request,"notes":all_item})
-
-
-@app.get('/register/{code_id}',response_model=Code)
+@app.get('/register/{code_id}')
 async def get_one(request:Request,code_id:int):
     query=notes.select().where(notes.c.id==code_id)
     print(query)
     uni_item=await database.fetch_one(query)
 
+    print(uni_item)
+    if uni_item is None:
+        return{
+            "code":"ok",
+            "message":"no notes found"
+        }
+    return {
+        "code":"ok",
+        "message":"success",
+        "user":uni_item['user'],
+        "expired":uni_item['expired'],
+        "mac_address":uni_item['mac_address']
+    }
 
-    return  templates.TemplateResponse('code_detail.html',{"request":request,"row":uni_item,"user":uni_item['user'],"code":uni_item['code'],"expired":uni_item['expired'],"mac_address":uni_item['mac_address']})
 
-@app.get('/update/{code_id}',response_model=Code)
-def jump_to_update(request:Request,code_id:int,row:list):
-    print("row=",row)
-    return  templates.TemplateResponse('code_update.html',{"request":request,"code_id":code_id,"row":uni_item})
-
-@app.put('/register/{code_id}',response_model=Code,response_model_include={"id", "user","code","expired","mac_address"})
-async def update(code_id:int ,r: CodeIn=Depends()):
-    code_arg = r.expired
-    code_arg+= r.mac_address
-    encode=encode_rsa(code_arg,r.mac_address)
-    
+@app.put('/register/{code_id}')
+async def update(code:CodeIn,code_id=int):
+    code_arg = code.expired
+    code_arg+= code.mac_address
+    encode=encode_rsa(code_arg,code.mac_address)
     query=notes.update().where(notes.c.id==code_id).values(
-        user=r.user,
-        expired=r.expired,
+        user=code.user,
+        expired=code.expired,
         code=encode,
-        mac_address=r.mac_address
+        mac_address=code.mac_address
     )
   
     record_id= await database.execute(query)
    
     query=notes.select().where(notes.c.id == record_id)
     user=await database.fetch_one(query)
-    return user
+    return {
+        "code":"ok",
+        "message":"success",
+        "expired":code.expired,
+        "mac_address":code.mac_address
+    }
 
-@app.get("/delete/{code_id}")
-def jump_to_confirm_delete(request:Request,code_id:int):
-    print("jump_to_confirm_delete")
-    return  templates.TemplateResponse('code_confirm_delete.html',{"request":request,"code_id":code_id})
 
-@app.delete("/register/{code_id}",response_model=Code)
+@app.delete("/register/{code_id}")
 async def delete(code_id:int):
     print("delete")
     query=notes.delete().where(notes.c.id == code_id)
     await database.execute(query)
-    return RedirectResponse('/')
-######
-@app.get("/code")
-def get_posts():
-    return codedb
-# ADD
-@app.post("/code")
-def add_post(code: Code):
-    code_arg = code.expired
-    code_arg+= code.mac_address
-    encode=encode_rsa(code_arg,code.mac_address)
-    code.code=encode
-    #generate_licensefile(encode)
-    codedb.append(code.dict())
-    return codedb[-1]
+    return {
+        "code":"ok",
+        "Message":"Deleted Code "+ str(code_id)
+    }
 
-@app.get("/code/{code_id}")
-def get_post(code_id: int):
-    code = code_id - 1
-    return codedb[code]
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("item.html", {"request": request, "id": id})
-
-# Update
-@app.post("/code/{code_id}")
-def update_post(code_id: int, code: Code):
-    
-    code.user=codedb[code_id]['user']
-    date= code.expired
-    machine_code= code.mac_address
-
-    code_arg = date
-    code_arg+= machine_code
-    encode=encode_rsa(code_arg,code.mac_address)
-    code.code=encode
-    #generate_licensefile(encode)
-    code.mac_address=codedb[code_id]['mac_address']
-    codedb[code_id] = code
-    
-    return {"message": "Code has been updated succesfully"}
-
-
-# Delete
-@app.delete("/code/{code_id}")
-def delete_post(code_id: int):
-    codedb.pop(code_id-1)
-    return {"message": "Post has been deleted succesfully"}
-
-
-
-@app.get("/items/")
-async def read_items():
-    html_content = """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look ma! HTML!</h1>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
-
-@app.get("/legacy/")
-def get_legacy_data():
-    data = """<?xml version="1.0"?>
-    <shampoo>
-    <Header>
-        Apply shampoo here.
-    </Header>
-    <Body>
-        You'll have to use soap here.
-    </Body>
-    </shampoo>
-    """
-    return Response(content=data, media_type="application/xml")
-
-
-
-
-@app.get('/download')
-def form_post(request: Request):
-    result = 'Type a number'
-    
-    return templates.TemplateResponse('/item.html', context={'request': request, 'result': result})
-
-@app.get('/test', response_class=HTMLResponse)
-def form_post(request: Request):
-    result = 'Type a number'
-    
-    return templates.TemplateResponse('/item.html', context={'request': request, 'result': result})
+@app.get("/download/{code_id}")
+async def download(code_id:int):
+    query=notes.select().where(notes.c.id==code_id)
+    print(query)
+    uni_item=await database.fetch_one(query)
+    generate_licensefile(uni_item.code)
+    return {
+        "code":"ok",
+        "Message":"Downloaded"
+    }
